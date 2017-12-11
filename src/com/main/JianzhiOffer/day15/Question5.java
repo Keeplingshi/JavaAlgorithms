19e2aea13e09160e9fe049b3911aa1d1df120ff9
chenbin <chenbkeep@163.com>
2017-12-11 20:38:36 +0800

    update

--- src/com/main/JianzhiOffer/day15/Question5.java
+++ src/com/main/JianzhiOffer/day15/Question5.java
@@ -1,9 +1,5 @@
 package com.main.JianzhiOffer.day15;
 
-import java.util.ArrayList;
-
-import com.main.JianzhiOffer.day15.Question4.ListNode;
-
 /**
  * 在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留，返回链表头指针。 例如，链表1->2->3->3->4->4->5
  * 处理后为 1->2->5
