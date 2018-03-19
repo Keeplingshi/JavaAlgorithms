34c94f35de0fd124368f5bc20f1eb476eaff6eb9
chenbin <chenbkeep@163.com>
2018-03-19 21:04:00 +0800

    update

--- src/com/algorithm/Sort.java
+++ src/com/algorithm/Sort.java
@@ -15,63 +15,45 @@ public class Sort {
 //		Sort.selectionSort(array);
 //		Sort.insertionSort(array);
 //		Sort.mergeSort(array);
-		Sort.quickSort(array);
+//		Sort.quickSort(array);
+		Sort.countSort(array);
 	}
 	
 	/**
-	 * 快速排序
-	 * @param array
+	 * 计数排序
+	 * @param a
+	 * @return
 	 */
-	private static void quickSort(int[] array) 
-	{
-		quickSort(array,0,array.length-1);
-		printArr(array);
-	}
-
-	/**
-	 * 
-	 * @param array
-	 * @param left
-	 * @param right
-	 */
-	private static void quickSort(int[] array, int left, int right) 
-	{
-		if(left>right){
-			return;
-		}
-		
-		int base=array[left];
-		int i=left;
-		int j=right;
-		
-		while(i!=j)
-		{
-			//找到第一个比基准小的值
-			while(array[j]>=base&&i<j){
-				j--;
-			}
-			//从左至右，找到第一个比基准大的值
-			while(array[i]<=base&&i<j)
-			{
-				i++;
-			}
-			
-			//交换这两个值得位置
-			if(i<j){
-				int temp=array[i];
-				array[i]=array[j];
-				array[j]=temp;
-			}
-		}
-		
-		//将base放到左右中间，此时i==j
-		array[left]=array[i];
-		array[i]=base;
-		
-		quickSort(array,left,i-1);
-		quickSort(array,i+1,right);
-	}
-
+    public static int[] countSort(int[] array){
+        int b[] = new int[array.length];
+        //先找数组中的最大值和最小值
+        int max = array[0];
+        int min = array[0];
+        for(int i:array){
+            if(i>max){
+                max=i;
+            }
+            if(i<min){
+                min=i;
+            }
+        }
+        
+        //这里k的大小是要排序的数组中，元素大小的极值差+1
+        int k=max-min+1;
+        int c[]=new int[k];
+        for(int i=0;i<array.length;++i){
+            c[array[i]-min]+=1;//优化过的地方，减小了数组c的大小
+        }
+        for(int i=1;i<c.length;++i){
+            c[i]=c[i]+c[i-1];
+        }
+        for(int i=array.length-1;i>=0;--i){
+            b[--c[array[i]-min]]=array[i];//按存取的方式取出c的元素
+        }
+        printArr(b);
+        return b;
+    }
+	
 	/**
 	 * 归并排序
 	 * @param array
@@ -83,24 +65,23 @@ public class Sort {
 	}
 
 	/**
-	 * 归并排序递归过程
+	 * 归并排序
 	 * @param array
-	 * @param i
-	 * @param j
+	 * @param start
+	 * @param end
 	 */
 	private static void mergeSort(int[] array, int start, int end) 
 	{
 		int mid=(start+end)/2;
-		if(start<end)
-		{
-			mergeSort(array, start, mid); 
+		if(start<end){
+			mergeSort(array,start,mid);
 			mergeSort(array,mid+1,end);
 			mergeSort(array,start,mid,end);
 		}
 	}
 
 	/**
-	 * 归并排序，对数组array的start到end进行排序
+	 * 归并排序，将start到end之间的数排序，其中start到mid与mid到end之间都是已排好序的
 	 * @param array
 	 * @param start
 	 * @param mid
@@ -108,33 +89,84 @@ public class Sort {
 	 */
 	private static void mergeSort(int[] array, int start, int mid, int end) 
 	{
-		int tmp[]=new int[end-start+1];
-		int left=start;		//数组array，从start到mid的左指针
-		int right=mid+1;	//数组array，从mid+1到end的右指针
-		int t=0;			//数组tmp的指针
+		int arrtmp[]=new int[end-start+1];
+		int left=start;		//左指针起点
+		int right=mid+1;	//右指针起点
+		int k=0;
 		
 		while(left<=mid&&right<=end)
 		{
-			if(array[left]<array[right]){
-				tmp[t++]=array[left++];
+			if(array[left]<=array[right]){
+				arrtmp[k++]=array[left++];
 			}else{
-				tmp[t++]=array[right++];
+				arrtmp[k++]=array[right++];
 			}
 		}
 		
-		//将array数组中剩余的部分放入tmp中
+		//将剩余数组放入arrtmp数组中
 		while(left<=mid){
-			tmp[t++]=array[left++];
+			arrtmp[k++]=array[left++];
 		}
 		while(right<=end){
-			tmp[t++]=array[right++];
+			arrtmp[k++]=array[right++];
 		}
 		
-		//将排序好的tmp数组放回到array中，array的下标为start到end
-		for(int i=0;i<tmp.length;i++)
-		{
-			array[i+start]=tmp[i];
+		for(int i=0;i<arrtmp.length;i++){
+			array[start+i]=arrtmp[i];
 		}
+		
+	}
+
+	/**
+	 * 快速排序
+	 * @param array
+	 */
+	private static void quickSort(int[] array) 
+	{
+		quickSort(array,0,array.length-1);
+		printArr(array);
+	}
+
+	/**
+	 * 快速排序
+	 * @param array
+	 * @param left
+	 * @param right
+	 */
+	private static void quickSort(int[] array, int left, int right) 
+	{
+		if(left>right){
+			return;
+		}
+		int base=array[left];
+		int i=left;
+		int j=right;
+		
+		while(i!=j)
+		{
+			//哨兵先从右侧开始走，找到第一个小于基准base的数
+			while(array[j]>=base&&i<j){
+				j--;
+			}
+			//哨兵从左侧开始走，找到第一个大于基准base的数
+			while(array[i]<=base&&i<j){
+				i++;
+			}
+			
+			//交换两个哨兵位置
+			if(i<j){
+				int temp=array[i];
+				array[i]=array[j];
+				array[j]=temp;
+			}
+		}
+		
+		//此时i==j，哨兵相遇，相遇位置与基准交换，则基准左侧都小于基准，右侧都大于基准
+		array[left]=array[i];
+		array[i]=base;
+		
+		quickSort(array,left,i-1);
+		quickSort(array,i+1,right);
 	}
 
 
