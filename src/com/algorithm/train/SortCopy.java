fa657106840abba158a6e3bbe406b417b7af9646
陈斌 <chenbin024@ljstaff.com>
2018-07-27 18:45:05 +0800

    fix

--- src/com/algorithm/train/SortCopy.java
+++ src/com/algorithm/train/SortCopy.java
@@ -18,101 +18,53 @@ public class SortCopy {
 		quickSort(array);
 //		countSort(array);
 	}
-	
-	/**
-	 * 快速排序
-	 * @param array
-	 */
-	private static void quickSort(int[] array) {
+
+	private static void quickSort(int[] array)
+	{
 		quickSort(array,0,array.length-1);
 		printArr(array);
 	}
 
-	/**
-	 * 快速排序
-	 * @param array
-	 * @param i
-	 * @param j
-	 */
-	private static void quickSort(int[] array, int start, int end) {
+	private static void quickSort(int[] array, int start, int end)
+	{
 		if(start>end){
 			return;
 		}
-		
 		int base=array[start];
-		int i=start;
-		int j=end;
-		
-		while(i!=j)
+		int left=start;
+		int right=end;
+
+		while (left != right)
 		{
-			while(i<j&&array[j]>=base){
-				j--;
+			while (left < right && array[right] >= base) {
+				right--;
 			}
-			while(i<j&&array[i]<=base){
-				i++;
+			while (left < right && array[left] <= base) {
+				left++;
 			}
-			
-			if(i<j){
-				int temp=array[i];
-				array[i]=array[j];
-				array[j]=temp;
+			if(left<right){
+				int temp = array[left];
+				array[left] = array[right];
+				array[right] = temp;
 			}
 		}
-		
-		//i==j
-		array[start]=array[i];
-		array[i]=base;
-		
-		quickSort(array, start, i-1);
-		quickSort(array,i+1,end);
+
+		array[start] = array[left];
+		array[left] = base;
+
+		quickSort(array, start, left - 1);
+		quickSort(array, left + 1, end);
 	}
 
-	/**
-	 * 插入排序
-	 * 插入排序的基本操作就是将一个数据插入到已经排好序的有序数据中，
-	 * 从而得到一个新的、个数加一的有序数据，算法适用于少量数据的排序，时间复杂度为O(n^2)。
-	 * 算法时间复杂度O(n2)
-	 * @param array
-	 */
-	private static void insertionSort(int[] array) 
-	{
-
-	}
-
-	/**
-	 * 冒泡排序：
-		在要排序的一组数中，对当前还未排好序的范围内的全部数，自上而下对相邻的两个数依次进行比较和调整，
-		让较大的数往下沉，较小的往上冒。即：每当两相邻的数比较后发现它们的排序与排序要求相反时，就将它们互换。
-		冒泡排序是稳定的。算法时间复杂度O(n^2)
-	 */
-	public static void bubbleSort(int[] array)
-	{
-
-	}
-	
-	/**
-	 * 选择排序
-	 * 它的工作原理是每一次从待排序的数据元素中选出最小（或最大）的一个元素，存放在序列的起始位置，直到全部待排序的数据元素排完。
-	 * 不断选择剩余数组中的最小的一个数
-	 * 算法复杂的O(n2)
-	 * @param array
-	 */
-	public static void selectionSort(int[] array)
-	{
-
-	}
-
-	
 	/**
 	 * 打印数组
-	 * @param array
+	 * @param array 需要打印的数组
 	 */
-	public static void printArr(int[] array)
+	private static void printArr(int[] array)
 	{
 		for (int i : array) {
 			System.out.print(i+" ");
 		}
 		System.out.println();
 	}
-	
 }
