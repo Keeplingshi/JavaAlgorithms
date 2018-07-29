e7086aff03e36066532ec6ebbbf2e0088794511e
chenbin <chenbkeep@163.com>
2018-07-29 16:37:10 +0800

    update

--- src/com/algorithm/train/SortCopy.java
+++ src/com/algorithm/train/SortCopy.java
@@ -14,9 +14,51 @@ public class SortCopy {
 //		bubbleSort(array);
 //		selectionSort(array);
 //		insertionSort(array);
-//		mergeSort(array);
-		quickSort(array);
+		mergeSort(array);
+//		quickSort(array);
 //		countSort(array);
+	}
+
+	private static void mergeSort(int[] array)
+	{
+		mergeSort(array,0,array.length-1);
+		printArr(array);
+	}
+
+	private static void mergeSort(int[] array, int start, int end)
+	{
+		int mid = (end - start) / 2;
+		if (start < mid) {
+			mergeSort(array, start, mid);
+			mergeSort(array, mid + 1, end);
+			mergeSort(array, start, mid, end);
+		}
+	}
+
+	//array数组中，start到mid是有序的，mid+1到end是有序的
+	private static void mergeSort(int[] array, int start, int mid, int end)
+	{
+		int temp[] = new int[end - start + 1];
+		int i = start;
+		int j = mid + 1;
+		int k = 0;
+
+		while (i <= mid && j <= end) {
+			if (array[i] < array[j]) {
+				temp[k++] = array[i++];
+			} else {
+				temp[k++] = array[j++];
+			}
+		}
+
+		while (i <= mid) {
+			temp[k++] = array[i++];
+		}
+		while (j <= end) {
+			temp[k++] = array[j++];
+		}
+
+		System.arraycopy(temp, 0, array, start, temp.length);
 	}
 
 	private static void quickSort(int[] array)
